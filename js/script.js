af4a76ba78de3bdbe8db632bcf3933e5f39d59e2
Romashka <rroma13579@gmail.com>
2022-08-14 23:51:56 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 var range_input = document.getElementById("rangeinput")
+var icon = document.getElementById("icon")
 function Value(val){
     if (val >= 0 && val <= 51){
         range_input.style.background = "rgb(255, "+(val*5)+", 0)"
@@ -12,19 +13,19 @@ function Value(val){
     else if (val > 153 && val <= 204){
         range_input.style.background = "rgb(0, "+(255 - ((val-153)*5))+", 255)"
     }
-    // if (val >= 0 && val < 20){
-    //     range_input.style.background = "red"
-    // }
-    // else if (val >= 20 && val < 40){
-    //     range_input.style.background = "blue"
-    // }
-    // else if (val >= 40 && val < 60){
-    //     range_input.style.background = "white"
-    // }
-    // else if (val >= 60 && val < 80){
-    //     range_input.style.background = "yellow"
-    // }
-    // else if (val >= 80 && val < 100){
-    //     range_input.style.background = "green"
-    // }
+    if (val >= 0 && val < 40.8){
+        icon.style.background = "url(img/red.svg)"
+    }
+    else if (val >= 40.8 && val < 81.6){
+        icon.style.background = "url(img/yellow.svg)"
+    }
+    else if (val >= 81.6 && val < 122.4){
+        icon.style.background = "url(img/green.svg)"
+    }
+    else if (val >= 122.4 && val < 163.2){
+        icon.style.background = "url(img/light_blue.svg)"
+    }
+    else if (val >= 163.2 && val < 204){
+        icon.style.background = "url(img/blue.svg)"
+    }
 }
